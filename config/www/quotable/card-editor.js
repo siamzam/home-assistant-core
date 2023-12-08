fdf21a13e399c62586639b477d13348b84f13944
Anastasia Koni <anastkoni@gmail.com>
2023-12-08 23:41:38 +0100

    Refactor to make code more testable

--- config/www/quotable/card-editor.js
+++ config/www/quotable/card-editor.js
@@ -6,9 +6,7 @@ class QuotableCardEditor extends HTMLElement {
     this._authors = [];
     this._selectedTags = [];
     this._selectedAuthors = [];
-    this._intervalValue = 300;
-    this._bgColor = "";
-    this._textColor = "";
+    this.intervalValue = 300;
   }
 
   set hass(hass) {
@@ -21,11 +19,16 @@ class QuotableCardEditor extends HTMLElement {
 
   async connectedCallback() {
     this.loadOptions();
+    this.addEventListener("config-changed", this.handleConfigChanged);
   }
-
+  handleConfigChanged(event) {
+    console.log("Configuration changed:", event.detail.config);
+  }
   async loadOptions() {
     try {
+      console.log("in load options");
       if (!this._config.entity) {
+        console.error("Entity not defined in configuration.");
         return;
       }
 
@@ -34,6 +37,7 @@ class QuotableCardEditor extends HTMLElement {
         entity_id: this._config.entity,
       };
 
+      //Message object used when calling quotable service
       const authorMessage = {
         domain: "quotable",
         service: "fetch_all_authors",
@@ -52,23 +56,33 @@ class QuotableCardEditor extends HTMLElement {
       };
 
       // Call quotable service to fetch all authors
-      const authorsResult = await this._hass.callWS(authorMessage);
-      this._authors = authorsResult.response.success
-        ? authorsResult.response.data
-        : [];
-
+      const responseAuthor = await this._hass.callWS(authorMessage);
       // Call quotable service to fetch all tags
-      const tagsResult = await this._hass.callWS(tagMessage);
-      this._tags = tagsResult.response.success ? tagsResult.response.data : [];
-    } catch (error) {
-      return;
-    }
+      const responseTags = await this._hass.callWS(tagMessage);
 
-    this._selectedBgColor =
-      this._hass.states[this._config.entity].attributes.styles.bg_color || "";
-    this._selectedTextColor =
-      this._hass.states[this._config.entity].attributes.styles.text_color || "";
+      if (responseAuthor && responseAuthor.response) {
+        // Update the authors property with the fetched authors
+        this._authors = Object.values(responseAuthor.response);
+      }
+
+      if (responseTags && responseTags.response) {
+        // Update the _tags property with the fetched tags
+        this._tags = Object.values(responseTags.response);
+      }
+    } catch (error) {
+      console.error("Error fetching options:", error);
+    }
     this.renderForm();
+  }
+
+  addRemoveListItems(_selectedList, selectedOption, selectedElement) {
+    const index = _selectedList.indexOf(selectedOption.value);
+    if (index === -1) {
+      _selectedList.push(selectedOption.value);
+    } else {
+      _selectedList.splice(index, 1);
+    }
+    return (selectedElement.textContent = _selectedList.join(", "));
   }
 
   //Render the visual representation
@@ -76,97 +90,83 @@ class QuotableCardEditor extends HTMLElement {
     // Add the container to the shadow DOM
     this.shadowRoot.innerHTML = `
 
-        <style>
-    div {
+    <style>
+      div {
         margin: 20px;
       }
 
-    select[multiple] {
-      width: 100%;
-      height: 100px;
-      border: 1px solid #ccc;
-      border-radius: 5px;
-      padding: 5px;
-      overflow-y: auto;
-    }
-    option {
-      padding: 5px;
-      cursor: pointer;
-    }
+      select[multiple] {
+        width: 100%;
+        height: 100px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        background-color: #fff;
+        padding: 5px;
+        overflow-y: auto;
+      }
+      option {
+        padding: 5px;
+        cursor: pointer;
+      }
 
-
-
-    input[type="text"] {
-      width: 100%;
-      padding: 5px;
-      margin-bottom: 10px;
-    }
-
-    input[type="range"] {
-      width: 80%;
-      height: 10px;
-      border: 1px solid #ccc;
-      border-radius: 5px;
-      background-color: #fdd835;
-      outline: none;
-    }
-
-
-    input[type="range"]::-webkit-slider-thumb {
-      -webkit-appearance: none;
-      width: 20px;
-      height: 20px;
-      background-color: #007BFF;
-      border: 1px solid #007BFF;
-      border-radius: 50%;
-      cursor: pointer;
-    }
+     
 
       input[type="text"] {
         width: 100%;
         padding: 5px;
         margin-bottom: 10px;
       }
-  </style>
 
-  <form id="form">
+      input[type="range"] {
+        width: 80%;
+        height: 10px;
+        border: 1px solid #ccc;
+        border-radius: 5px;
+        background-color: #fdd835;
+        outline: none;
+      }
 
 
-  <div style="display: flex; align-items: center;">
-    <label for="backgroundColorPicker" style="margin-right: 10px;">Select Card Background Color:</label>
-    <input type="color" id="backgroundColorPicker" value=${
-      this._selectedBgColor
-    }>
-    <label for="TextColorPicker" style="margin-left: 20px; margin-right: 10px;">Select Quote Text Color:</label>
-    <input type="color" id="textColorPicker" value=${this._selectedTextColor}>
-  </div>
+      input[type="range"]::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        width: 20px;
+        height: 20px;
+        background-color: #007BFF;
+        border: 1px solid #007BFF;
+        border-radius: 50%;
+        cursor: pointer;
+      }
 
+      input[type="text"] {
+        width: 100%;
+        padding: 5px;
+        margin-bottom: 10px;
+      }
+
+
+
+    </style>
+      <form id="form">
     <div>
     <label for="authorSelect">Select Authors:</label>
-    <span id="selectedAuthorsLabel"></span>
     <input type="text" id="authorInput" placeholder="Search here">
     <select id="authorSelect" multiple>
       ${this._authors
-        .map(
-          (author) =>
-            `<option data-name="${author.name}" data-slug="${author.slug}" value="${author.slug}">${author.name}</option>`
-        )
+        .map((author) => `<option value="${author}">${author}</option>`)
         .join("")}
     </select>
+    <span id="selectedAuthorLabel"></span>
+
   </div>
 
   <div>
     <label for="tagSelect">Select Categories:</label>
-    <span id="selectedTagsLabel"></span>
-    <input type="text" id="tagInput" placeholder="Select from list">
+    <input type="text" id="selectedTags" readonly  placeholder="Select from list">
     <select id="tagSelect" multiple>
-    ${this._tags
-      .map(
-        (tag) =>
-          `<option data-name="${tag.name}" data-slug="${tag.slug}" value="${tag.slug}">${tag.name}</option>`
-      )
-      .join("")}
-  </select>
+      ${this._tags
+        .map((tag) => `<option value="${tag}">${tag}</option>`)
+        .join("")}
+    </select>
   </div>
 
   <div>
@@ -180,22 +180,15 @@ class QuotableCardEditor extends HTMLElement {
     // Add references to the input and multiselect elements
     const authorInput = this.shadowRoot.getElementById("authorInput");
     const authorSelect = this.shadowRoot.getElementById("authorSelect");
-    const tagInput = this.shadowRoot.getElementById("tagInput");
+    const selectedTags = this.shadowRoot.getElementById("selectedTags");
     const tagSelect = this.shadowRoot.getElementById("tagSelect");
     const updateIntervalSlider = this.shadowRoot.getElementById("slider");
     const updateIntervalLabel = this.shadowRoot.getElementById(
       "updateIntervalLabel"
     );
-    const selectedAuthorsLabel = this.shadowRoot.getElementById(
-      "selectedAuthorsLabel"
+    const selectedAuthorLabel = this.shadowRoot.getElementById(
+      "selectedAuthorLabel"
     );
-    const selectedTagsLabel =
-      this.shadowRoot.getElementById("selectedTagsLabel");
-    const form = this.shadowRoot.getElementById("form");
-    const bgColorPicker = this.shadowRoot.getElementById(
-      "backgroundColorPicker"
-    );
-    const textColorPicker = this.shadowRoot.getElementById("textColorPicker");
 
     // Add  event listener to search author
     authorInput.addEventListener("keyup", () => {
@@ -204,94 +197,74 @@ class QuotableCardEditor extends HTMLElement {
 
     // Add click event listener to update selected author list
     authorSelect.addEventListener("click", (event) => {
-      const authorOption = event.target;
+      const selectedOption = event.target;
 
-      if (authorOption.tagName === "OPTION") {
+      if (selectedOption.tagName === "OPTION") {
         // Toggle the background color of the selected option
-        authorOption.style.backgroundColor =
-          authorOption.style.backgroundColor === "#007BFF" ? "#fff" : "#007BFF";
+        selectedOption.style.backgroundColor =
+          selectedOption.style.backgroundColor === "#007BFF"
+            ? "#fff"
+            : "#007BFF";
 
         // Toggle the text color of the selected option
-        authorOption.style.color =
-          authorOption.style.color === "#fff" ? "#007BFF" : "#fff";
+        selectedOption.style.color =
+          selectedOption.style.color === "#fff" ? "#007BFF" : "#fff";
 
-        // Add or remove the selected item from the lists
-        const authorIndex = this._selectedAuthors.findIndex(
-          (author) => author.slug == authorOption.dataset.slug
+        // Add or remove the selected item from the list
+        addRemoveListItems(
+          _selectedAuthors,
+          selectedOption,
+          selectedAuthorLabel
         );
-        if (authorIndex >= 0) {
-          this._selectedAuthors.splice(authorIndex, 1);
-        } else {
-          this._selectedAuthors.push({
-            name: authorOption.dataset.name,
-            slug: authorOption.dataset.slug,
-          });
-        }
-
-        // Update the selected author list
-        selectedAuthorsLabel.textContent = this._selectedAuthors
-          .map((author) => author.name)
-          .join(", ");
       }
     });
 
     // Add click event listener to update selected tags list
     tagSelect.addEventListener("click", (event) => {
-      const tagOption = event.target;
+      const selectedOption = event.target;
 
-      if (tagOption.tagName === "OPTION") {
+      if (selectedOption.tagName === "OPTION") {
         // Toggle the background color of the selected option
-        tagOption.style.backgroundColor =
-          tagOption.style.backgroundColor === "#007BFF" ? "#fff" : "#007BFF";
+        selectedOption.style.backgroundColor =
+          selectedOption.style.backgroundColor === "#007BFF"
+            ? "#fff"
+            : "#007BFF";
 
         // Toggle the text color of the selected option
-        tagOption.style.color =
-          tagOption.style.color === "#fff" ? "#007BFF" : "#fff";
+        selectedOption.style.color =
+          selectedOption.style.color === "#fff" ? "#007BFF" : "#fff";
 
-        // Add or remove the selected item from the lists
-        const tagIndex = this._selectedTags.findIndex(
-          (tag) => tag.slug == tagOption.dataset.slug
-        );
-        if (tagIndex >= 0) {
-          this._selectedTags.splice(tagIndex, 1);
-        } else {
-          this._selectedTags.push({
-            name: tagOption.dataset.name,
-            slug: tagOption.dataset.slug,
-          });
-        }
-
-        // Update the selected tags input
-        selectedTagsLabel.textContent = this._selectedTags
-          .map((tag) => tag.name)
-          .join(", ");
+        // Add or remove the selected item from the list
+        addRemoveListItems(_selected_Tags, selectedOption, selectedTags);
       }
     });
 
-    // Add input event listener to  interval slider
+    // Add input event listener to update interval slider
     updateIntervalSlider.addEventListener("input", () => {
       updateIntervalLabel.textContent = updateIntervalSlider.value;
       this.intervalValue = updateIntervalSlider.value;
     });
-
-    // Add event listeners for color pickers
-    bgColorPicker.addEventListener("input", () => {
-      this._selectedBgColor = bgColorPicker.value;
-    });
-
-    textColorPicker.addEventListener("input", () => {
-      this._selectedTextColor = textColorPicker.value;
-    });
-
-    form.addEventListener("focusout", this.updateConfiguration.bind(this));
   }
 
+  toggleOptionBackground(selectedOption) {
+    // Toggle the background color of the selected option
+    selectedOption.style.backgroundColor =
+      selectedOption.style.backgroundColor === "#007BFF" ? "#fff" : "#007BFF";
+
+    // Toggle the text color of the selected option
+    selectedOption.style.color =
+      selectedOption.style.color === "#fff" ? "#007BFF" : "#fff";
+  }
+
+  // Function for search_author service
   async searchAuthor(query) {
     try {
       const searchData = {
         entity_id: this._config.entity,
         query: query,
       };
+
+      console.log(searchData);
 
       const searchMessage = {
         domain: "quotable",
@@ -301,16 +274,19 @@ class QuotableCardEditor extends HTMLElement {
         service_data: searchData,
       };
 
-      const searchResult = await this._hass.callWS(searchMessage);
-      this._authors = searchResult.result.success
-        ? searchResult.result.data
-        : [];
+      const authorResponse = await this._hass.callWS(searchMessage);
 
-      if (this._authors.length >= 0) {
+      if (authorResponse && authorResponse.response) {
+        console.log(authorResponse.response);
         const authorSelect = this.shadowRoot.getElementById("authorSelect");
+
         // Clear existing options
         authorSelect.innerHTML = "";
-        // Add new options based on the author array
+        this._authors = Object.keys(authorResponse.response).map((slug) => ({
+          slug: slug,
+          name: authorResponse.response[slug],
+        }));
+        // Add new options based on the new author array
         this._authors.forEach((author) => {
           const option = document.createElement("option");
           option.value = author.slug;
@@ -319,64 +295,8 @@ class QuotableCardEditor extends HTMLElement {
         });
       }
     } catch (error) {
-      return;
+      console.error("Error searching author:", error);
     }
-  }
-
-  async updateConfiguration() {
-    try {
-      const updateConfigData = {
-        entity_id: this._config.entity,
-        selected_tags: this._selectedTags.map((tag) => tag.slug),
-        selected_authors: this._selectedAuthors.map((author) => author.slug),
-        update_frequency: parseInt(this._intervalValue) * 60,
-        styles: {
-          bg_color: this._selectedBgColor,
-          text_color: this._selectedTextColor,
-        },
-      };
-
-      const updateConfigMessage = {
-        domain: "quotable",
-        service: "update_configuration",
-        type: "call_service",
-        return_response: false,
-        service_data: updateConfigData,
-      };
-
-      const responseUpdateConfig = await this._hass.callWS(updateConfigMessage);
-
-      if (responseUpdateConfig) {
-        const newConfig = this._config;
-        const event = new CustomEvent("config-changed", {
-          detail: { config: newConfig },
-          bubbles: true,
-          composed: true,
-        });
-        this.dispatchEvent(event);
-        this.fetchQuote();
-      }
-    } catch (error) {
-      return;
-    }
-  }
-
-  async fetchQuote() {
-    const fetchData = {
-      entity_id: this._config.entity,
-    };
-
-    //Message object used when calling quotable service
-    const fetchNew = {
-      domain: "quotable",
-      service: "fetch_a_quote",
-      type: "call_service",
-      return_response: true,
-      service_data: fetchData,
-    };
-
-    // Call quotable service to fetch new quote
-    await this._hass.callWS(fetchNew);
   }
 }
 
