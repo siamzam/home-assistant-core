b437c7a06c2060cce6226a0eff40b012b73930d3
Michiale <meselhadgu@gmai.com>
2023-12-13 22:19:29 +0000

    Add tests for hass and config instances

--- config/www/quotable/__tests__/card-editor.test.js
+++ config/www/quotable/__tests__/card-editor.test.js
@@ -14,4 +14,27 @@ describe("QuotableCardEditor", () => {
     expect(quotableEditor).toBeDefined();
     expect(quotableEditor instanceof QuotableCardEditor).toBeTruthy();
   });
+
+  test("hass setter sets _hass", () => {
+    const mockHass = {
+      callWS: jest.fn().mockImplementation(() =>
+        Promise.resolve({
+          response: {
+            success: true,
+            data: ["hass"],
+          },
+        })
+      ),
+    };
+    quotableEditor.hass = mockHass;
+    expect(quotableEditor).toHaveProperty("_hass", mockHass);
+  });
+
+  test("setConfig sets _config", () => {
+    const mockConfig = {
+      entity: "quotable.quotable",
+    };
+    quotableEditor.setConfig(mockConfig);
+    expect(quotableEditor).toHaveProperty("_config", mockConfig);
+  });
 });
